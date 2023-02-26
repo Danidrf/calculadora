efd559ef4c55028af789892083b1eda089530691
Daniel Ramos Fernández <danielrfe80@educastur.es>
2023-02-26 12:35:18 +0100

    primera validacion

---
+++ src/calculadora/app_calculadora.java
@@ -0,0 +1,54 @@
+package calculadora; 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+	public class app_calculadora {
+			public static void main(String[] args) {
+					Scanner teclado;
+					teclado= new Scanner(System.in);
+					double num1, num2;
+					String operacion;
+					String respu;
+					char letrarespu;
+					double resul=0;
+					cal c;
+						do{
+							c=new cal();
+							num1=c.leer();
+							num2=c.leer();
+							c.setNum1(num1);
+							c.setNum2(num2);
+							operacion=c.calculo();
+							switch (operacion){
+								case "+":
+									resul=c.sumar();
+									break;
+								case "-":
+									resul=c.restar();
+									break;
+								case "*":
+									resul=c.multi();
+									break;
+								case "/":
+									resul=c.divi();
+									break;
+								case "^":
+									resul=c.potenciacion(); 
+									break; 
+								case "raiz": 
+									resul=c.raiz(); 
+									break;
+												}
+							JOptionPane.showMessageDialog(null,c.getNum1()+operacion+c.getNum2()+" = "+ resul);
+								respu=JOptionPane.showInputDialog("Quieres realizar otra operación (S/N): ");
+								letrarespu = respu.charAt(0);
+								letrarespu = Character.toUpperCase(letrarespu );
+									while ((letrarespu != 'S') && (letrarespu != 'N')){	
+											System.out.println("Error teclea (S/N): ");
+											respu= JOptionPane.showInputDialog("Error teclea (S/N): ");
+											letrarespu = respu.charAt(0);
+											letrarespu = Character.toUpperCase(letrarespu);
+									}
+						}while (letrarespu == 'S');
+						JOptionPane.showMessageDialog(null, "Fin Programa");
+}
+ }
---
+++ src/calculadora/cal.java
@@ -0,0 +1,71 @@
+package calculadora;
+import javax.swing.JOptionPane;
+public class cal {
+	private double num1;
+	private double num2; 
+	
+	public cal() {
+		super();
+	}
+
+	public double getNum1() {
+		return num1;
+	}
+
+	public void setNum1(double num1) {
+		this.num1 = num1;
+	}
+
+	public double getNum2() {
+		return num2;
+	}
+
+	public void setNum2(double num2) {
+		this.num2 = num2;
+	}
+	public double sumar() { 
+		double resultado; 
+		resultado=num1 + num2; 
+		return resultado;
+	}
+	public double restar() { 
+		double resultado; 
+		resultado=num1-num2; 
+		return resultado; 
+	}
+	public double multi() { 
+		double resultado; 
+		resultado=num1*num2; 
+		return resultado; 
+	}
+	public double divi() { 
+		double resultado; 
+		resultado=num1/num2; 
+		return resultado; 
+	}
+	public double potenciacion() {
+		double potencia; 
+		potencia=Math.pow(num1,num2); 
+		return potencia;
+	}
+	public double raiz() { 
+		double raiz; 
+		raiz=Math.pow(num1, 1/num2);
+		return raiz;
+	}
+	public void visualizar() { 
+		JOptionPane.showMessageDialog(null,"Calculadora Simple"); 
+		
+	} 
+	public double leer() {
+		double n; 
+		String info=JOptionPane.showInputDialog("Introduzca un número");
+		n=Double.parseDouble(info);
+		return n;
+	} 
+	public String calculo(){
+		String opera; 
+		opera=JOptionPane.showInputDialog(null,"Introduzca la operación que se desea realizar: "); 
+		return opera;	
+	}
+}
